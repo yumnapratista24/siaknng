c0ea549e466d4743d08a331270439b3eb9f074b5
hashlash <muh.ashlah@gmail.com>
2019-12-07 21:53:43 +0700

    1.0.5 rename current postgres kubernetes config to postgres-auth-*

--- siaknng/auth/siaknngauth/settings.py
+++ siaknng/auth/siaknngauth/settings.py
@@ -95,11 +95,11 @@ if 'DATABASE_URL' in os.environ:
 if all(key in os.environ for key in ['POSTGRES_USER', 'POSTGRES_PASSWORD']):
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('POSTGRES_NAME', 'postgres_django'),
+        'NAME': os.environ.get('POSTGRES_NAME', 'postgres'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('POSTGRES_SERVICE_HOST', 'postgres'),
-        'PORT': os.environ.get('POSTGRES_SERVICE_PORT', 5432),
+        'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),
     }
 
 
